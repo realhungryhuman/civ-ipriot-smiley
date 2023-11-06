fc1886949df52c97b2a87ebb1f27ebaa52b90b47
Dylan McClarence <20111795@tafe.wa.edu.au>
2023-11-06 18:51:11 +0800

    Implement Dynamic Complexion

--- smiley.py
+++ smiley.py
@@ -6,13 +6,15 @@ class Smiley:
     GREEN = (0, 255, 0)
     RED = (255, 0, 0)
     YELLOW = (255, 255, 0)
+    BLUE = (0, 0, 255)
     BLANK = (0, 0, 0)
 
-    def __init__(self):
+    def __init__(self, complexion=YELLOW):
         # We have encapsulated the SenseHat object
         self.sense_hat = SenseHat()
 
-        Y = self.YELLOW
+        self.my_complexion = complexion
+        Y = complexion
         O = self.BLANK
         self.pixels = [
             O, Y, Y, Y, Y, Y, Y, O,
@@ -37,3 +39,6 @@ class Smiley:
         Show the smiley on the screen.
         """
         self.sense_hat.set_pixels(self.pixels)
+
+    def complexion(self):
+        return self.my_complexion
